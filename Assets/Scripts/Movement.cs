6d0716427a95e497866ed5609bfe67aa94519487
DeclanHarty <declanharty@declans-air.wireless.rit.edu>
2024-01-24 14:54:35 -0500

    Added Jumping and Proper Collision

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,24 +9,55 @@ public class Movement : MonoBehaviour
 
     [SerializeField] private float verticalVelocity = 0;
     private const float TERMINAL_VELOCITY = -60;
+
+    [SerializeField] private float jumpVelocity;
+    [SerializeField] private Vector3 boxSize;
+
+    [SerializeField] private float castDistance;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+
+
+
+     
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void Move(){
         float direction = Input.GetAxisRaw("Horizontal");
 
-        if (verticalVelocity > TERMINAL_VELOCITY) { 
+        if (verticalVelocity > TERMINAL_VELOCITY && !IsGrounded()) { 
             verticalVelocity += gravityAccel * Time.deltaTime;
         }
 
-        transform.position += new Vector3(direction * speed * Time.deltaTime, verticalVelocity * Time.deltaTime, 0);
+        if(IsGrounded()){
+            if(Input.GetKeyDown("space")){
+                verticalVelocity = jumpVelocity;
+            }else if(verticalVelocity < 0){
+                verticalVelocity = 0;
+            }
+            
+        }
+
+        rb.position += new Vector2(direction * speed * Time.deltaTime, verticalVelocity * Time.deltaTime);
     }
 
-    public void GroundCheck(){
-        
+    private bool IsGrounded(){
+        if(Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    void OnDrawGizmos(){
+        Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
     }
 
 
